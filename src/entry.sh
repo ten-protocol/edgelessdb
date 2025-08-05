c71ebf5d7876407b8272f8f1d3fec0b5ddb0d317
Tudor Malene <tudor.malene@gmail.com>
2025-08-05 09:42:21 +0100

    fix entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
 set -e
+#
+# This script is the entry point for starting the enclave under a Docker container.
+# It allows running SGX sdk using different parameters.
+#
 
+# It's expected to be a link between the /dev/sgx_enclave Docker device and the container /dev/sgx/enclave
 mkdir -p /dev/sgx
 if [ ! -L /dev/sgx/enclave ]; then
 	ln -s /dev/sgx_enclave /dev/sgx/enclave
 fi
 
-if [ -n "${PCCS_ADDR}" ]; then
-	PCCS_URL=https://${PCCS_ADDR}/sgx/certification/v3/
-fi
+PCCS_URL=https://global.acccache.azure.net/sgx/certification/v4/
+echo "PCCS_URL: ${PCCS_URL}"
 
-if [ -z "${PCCS_URL}" ] && [ "$(cat /sys/devices/virtual/dmi/id/chassis_asset_tag)" != 7783-7084-3265-9085-8269-3286-77 ]; then
-	PCCS_URL=https://172.17.0.1:8081/sgx/certification/v3/
-fi
+apt-get install -qq libsgx-dcap-default-qpl
 
-if [ -n "${PCCS_URL}" ]; then
-	apt-get install -qq libsgx-dcap-default-qpl
-	echo "PCCS_URL: ${PCCS_URL}"
-	echo "PCCS_URL=${PCCS_URL}\nUSE_SECURE_CERT=FALSE" > /etc/sgx_default_qcnl.conf
-else
-	apt-get install -qq az-dcap-client
-fi
+echo "PCCS_URL=${PCCS_URL}\nUSE_SECURE_CERT=FALSE" > /etc/sgx_default_qcnl.conf
 
-./edb "$@"
+"$@"
